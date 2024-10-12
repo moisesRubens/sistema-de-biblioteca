437639314b8729c68893dee691d910c020efc4e9
moisesRubens <moisesrubens.pro@gmail.com>
2024-10-12 18:46:07 -0300

    second command

---
+++ Livro.java
@@ -0,0 +1,11 @@
+public class Livro {
+    private String name;
+    private String author;
+    private boolean available;
+
+    public Livro(String name, String author) {
+        this.name = name;
+        this.author = author;
+        this.available = true;
+    }
+}
