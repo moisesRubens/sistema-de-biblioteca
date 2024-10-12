3df74f98ecee86e28059f41c1a13678c48e29e6f
moisesRubens <moisesrubens.pro@gmail.com>
2024-10-12 20:59:17 -0300

    command 3

--- Livro.java
+++ Livro.java
@@ -4,8 +4,41 @@ public class Livro {
     private boolean available;
 
     public Livro(String name, String author) {
+        this.setName(name);
+        this.setAuthor(author);
+        this.setAvailable(true);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
         this.author = author;
-        this.available = true;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro: \n"+
+                "Titulo: "+ name +
+                "\nAutor: "+ author +
+                "\nDisponibilidade: " + available;
+
     }
 }
--- main.java
+++ main.java
@@ -1,4 +1,7 @@
 public class main {
     public static void main(String[] args) {
+        Livro livro = new Livro("Dexter", "Jeff L.");
+
+        System.out.println(livro.toString());
     }
 }
